c8035a2dcc7dcc49f363db9eed05bdd22c694b19
Aman Chaurasia <134186994+amanchaurasia07@users.noreply.github.com>
2023-11-16 11:29:10 +0530

    Update typeform_data_fetch.py

--- typeform_data_fetch.py
+++ typeform_data_fetch.py
@@ -1,1 +1,20 @@
+import json
+import requests
+from typing import List, Dict, Any
+import pandas as pd
+from datetime import datetime
 
+def list_workspace_forms(workspace_id, token: str):
+    header = {"Authorization": "Bearer {}".format(token)}
+    workspace_json = requests.get(
+        f"https://api.typeform.com/forms?workspace_id={workspace_id}",
+        params={"page": 1, "page_size": 200},
+        headers=header,
+    )
+    form_list = workspace_json.json()["items"]
+    return [{"id": form["id"], "title": form["title"]} for form in form_list]
+token = "tfp_9mtnXQToJNxPLMVaJtMgw2pD4Hkutujjd2RGPuqyvzCw_3mM5QCak5BWSfk"
+
+workspace_id = "9uPaNw"
+forms = list_workspace_forms(workspace_id, token)
+print(forms)
